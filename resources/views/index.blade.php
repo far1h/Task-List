0cfb4728ea6756c1d78e6b4a8b9faadc252f7222
far1h <farihmuhammad45@gmail.com>
2024-09-28 20:46:24 +0700

    Try display foreach task titles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,9 @@
 
 <div>
     @if (count($tasks))
-        <div>There are tasks!</div>        
+        @foreach ($tasks as $task)
+            <div>{{$task->title}}</div>            
+        @endforeach
     @else 
         <div>There are no tasks!</div>
     @endif
