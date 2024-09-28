3ea34862232122c7a74e4c9c60fc729be0bb3908
far1h <farihmuhammad45@gmail.com>
2024-09-28 21:24:09 +0700

    Display task details

--- routes/web.php
+++ routes/web.php
@@ -66,8 +66,12 @@ Route::get('/tasks', function () use ($tasks){
     ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id){
-    return "One single task";
+Route::get('/tasks/{id}', function ($id) use ($tasks) {
+    $task = collect($tasks)->firstWhere('id', $id);
+    if (! $task) {
+        abort(404);
+    }
+    return view('show', ['task' => $task]);
 })->name("tasks.show");
     
 // blade templates: used to render dynamic content that can differ depending on the data
