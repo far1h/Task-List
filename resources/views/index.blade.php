4a6e1f8e3017f07e5f08111098d714ada2b898b9
far1h <farihmuhammad45@gmail.com>
2024-09-28 21:33:13 +0700

    Implement layouts for task show pages

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,10 @@
     {{-- @if (count($tasks)) --}}
     @forelse ($tasks as $task)
         <div>
-        <a href="{{ route('tasks.show', ['id' => $task->id])}}">{{$task->title}}</a>    
-        </div>            
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
     @empty
         <div>There are no tasks!</div>
     @endforelse
     {{-- @endif --}}
 </div>
-
