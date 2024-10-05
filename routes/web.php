af048ad1f7d92606ae03aba98f9d9c9009340810
far1h <farihmuhammad45@gmail.com>
2024-10-05 14:08:10 +0700

    add form requests, add mass assignment

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task as ModelsTask;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -35,36 +36,26 @@ Route::get('/tasks/{task}', function (ModelsTask $task)  {
     ]);
 })->name("tasks.show");
 
-Route::put("/tasks/{task}", function (ModelsTask $task, Request $request) {
-    $data = $request->validate([
-        "title"=> "required|max:255",
-        "description"=> "required",
-        "long_description"=> "required",
-    ]);
-
-    $task->title = $data["title"];
-    $task->description = $data["description"];
-    $task->long_description = $data["long_description"];
-    $task->save();
-
-    return redirect()->route("tasks.show", ["id"=> $task->id])->with("success","Task updated successfully!");
-})->name('tasks.update');
-
-Route::post("/tasks", function (Request $request) {
-    $data = $request->validate([
-        "title"=> "required|max:255",
-        "description"=> "required",
-        "long_description"=> "required",
-    ]);
-
-    $task = new ModelsTask;
-    $task->title = $data["title"];
-    $task->description = $data["description"];
-    $task->long_description = $data["long_description"];
-    $task->save();
-
-    return redirect()->route("tasks.show", ["id"=> $task->id])->with("success","Task created successfully!");
+Route::post("/tasks", function (TaskRequest $request) {
+    // $data = $request->validated();
+    // $task = new ModelsTask;
+    // $task->title = $data["title"];
+    // $task->description = $data["description"];
+    // $task->long_description = $data["long_description"];
+    // $task->save();
+    $task = ModelsTask::create($request->validated());
+    return redirect()->route("tasks.show", ["task"=> $task->id])->with("success","Task created successfully!");
 })->name('tasks.store');
+
+Route::put("/tasks/{task}", function (ModelsTask $task, TaskRequest $request) {
+    // $data = $request->validated();
+    // $task->title = $data["title"];
+    // $task->description = $data["description"];
+    // $task->long_description = $data["long_description"];
+    // $task->save();
+    $task->update($request->validated());
+    return redirect()->route("tasks.show", ["task"=> $task->id])->with("success","Task updated successfully!");
+})->name('tasks.update');
 
 // blade templates: used to render dynamic content that can differ depending on the data
 
